f5fbc7a665b275745645d909c4984f4795262e8b
Hari-repo <=>
2025-05-01 22:45:26 +0530

    New another main file created with static blocks

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    public static void main(String[] args) {
 
         System.out.println("Welcome to the world of java");
     }
 }
+
 
---
+++ src/MainTest.java
@@ -0,0 +1,12 @@
+public class MainTest {
+    static{
+        System.out.println("Static 1");
+    }
+    static {
+        System.out.println("Static 2");
+    }
+    public static void main(String[] args) {
+        System.out.println("Gotcha new main program");
+    }
+
+}
