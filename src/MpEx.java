999fe7d64486054dd4754f9d16838fcaf5200655
Hari-repo <=>
2025-05-03 12:34:34 +0530

    Hashmap lambda example included

---
+++ src/MpEx.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MpEx {
+
+    public static void main(String[] args) {
+        Map<Integer,String> hm = new HashMap<>();
+        hm.put(1,"AWS");
+        hm.put(2,"GCP");
+        hm.put(3,"Azure");
+        System.out.println("Map Size :"+hm.size());
+        System.out.println("Map iteration using for loop");
+        for(Map.Entry<Integer,String> e : hm.entrySet()){
+            System.out.println("Key :"+ e.getKey() +"<----> Value :"+ e.getValue());
+        }
+        System.out.println("Map iteration using lambda java 8");
+        hm.forEach((key,val) -> {
+            System.out.println("Key :"+ key +"<----> Value :"+ val);
+        });
+        System.out.println("Map iterate using keyset");
+        for(Integer key : hm.keySet()){
+            System.out.println("Key :"+ key +"<----> Value :"+ hm.get(key));
+        }
+        System.out.println("Map iterate using values");
+        for(String val : hm.values()){
+            System.out.println("Values :"+ val );
+        }
+
+    }
+}
