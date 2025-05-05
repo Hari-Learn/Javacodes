87de93142fa7f9367ef0e11b178648950bbb9e54
Hari-repo <hariharanuk810@gmail.com>
2025-05-05 22:39:06 +0530

    Simple Hashmap exampl

---
+++ src/HTEx.java
@@ -0,0 +1,13 @@
+import java.util.Hashtable;
+
+public class HTEx {
+    public static void main(String[] args) {
+        Hashtable<Integer,String> student = new Hashtable();
+        student.put(1,"Alan");
+        student.put(2,"Barry");
+        student.put(3,"Charlie");
+        for(Integer i : student.keySet()){
+            System.out.println(i +"<-K - V-> >"+student.get(i));
+        }
+    }
+}
