96e715c8d86975f72969f2a710d6132360ea2baf
Hari-repo <hariharanuk810@gmail.com>
2025-05-10 08:12:15 +0530

    Using streams added in set example

--- src/LHSEx.java
+++ src/LHSEx.java
@@ -1,5 +1,7 @@
+import java.util.Arrays;
 import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class LHSEx {
     public static void main(String[] args) {
@@ -11,7 +13,24 @@ public class LHSEx {
         System.out.println("Using lambda ");
         lhs.forEach(strs -> System.out.println("--->"+strs));
         System.out.println(lhs.size());
+        System.out.println("Using Streams");
+        Set<String> lhashSet = Arrays.stream(str.split(" ")).collect(Collectors.toCollection(LinkedHashSet::new));
+        lhashSet.forEach(strs -> System.out.println("->>>" + strs));
 
+        System.out.println("Using peek in streams");
+        Set<String> lset = Arrays.stream(str.split(" "))
+                .peek(word -> System.out.println("Inside streams :"+word))
+                .collect(Collectors.toCollection(LinkedHashSet::new));
+
+        System.out.println("Using map and filter");
+        Set<String> lhss = Arrays.stream(str.split( " "))
+                .map(String::toUpperCase)
+                .peek(word-> {
+                    System.out.print(" "+word);
+                })
+                .filter(ws -> !ws.equals("AND"))
+
+                .collect(Collectors.toCollection(LinkedHashSet::new));
 
     }
 }
