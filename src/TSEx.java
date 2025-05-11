7a79585ca83123e44815824d3c3630253f7388c9
Hari-repo <hariharanuk810@gmail.com>
2025-05-11 23:55:16 +0530

    Treeset example added

---
+++ src/TSEx.java
@@ -0,0 +1,18 @@
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TSEx {
+    public static void main(String[] args) {
+        Set<String> tset = new TreeSet<>();
+        String str = "I am learning devops so i started first linux aws docker then kubernetes. In near future i will learn monitor tools and helm too";
+        for(String s : str.split(" ")){
+            //System.out.println(s);
+            tset.add(s);
+        }
+        System.out.println("Iterating treeset");
+        for(String st : tset){
+            System.out.println(st);
+        }
+
+    }
+}
