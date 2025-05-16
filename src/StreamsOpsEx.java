4f15574f4948d55bd80834b7ee1271acdbe7749b
Hari-repo <hariharanuk810@gmail.com>
2025-05-16 22:41:52 +0530

    Streams Finding first match added

--- src/StreamsOpsEx.java
+++ src/StreamsOpsEx.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class StreamsOpsEx {
     public static void main(String[] args) {
@@ -7,6 +8,24 @@ public class StreamsOpsEx {
         System.out.println("Using noneMatch");
         boolean hasNoNegative = list.stream().noneMatch(n->n<0);
         System.out.println(hasNoNegative);
+        List<String> al = Arrays.asList("Ambi","Ambal","Ammu","Anbu","Arivu","Balu");
+
+        boolean test = al.stream().filter(s->s.startsWith("A"))
+                                  .filter(s->s.length()<=4)
+                                  .peek(s -> System.out.println(s))
+                                  .noneMatch(s->s.equals("Anbu"));
+        System.out.println(test);
+
+        System.out.println("Using findFirst");
+        List<String> as = Arrays.asList("Ambi","Ajju","Cathe","Diana","Eille","Fathima","Balu");
+        Optional<String> findingFirstA = as.stream()
+                .filter(s -> s.startsWith("A"))
+                 .findFirst();
+        System.out.println(findingFirstA);
+        System.out.println("Using contain with findFirst");
+        Optional<String> findingAny = as.stream().filter(s -> s.contains("na")).findFirst();
+        findingAny.ifPresent(s -> System.out.println("Gotcha :"+s));
+
 
     }
 }
