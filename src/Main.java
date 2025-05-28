d706a9b0bcaa8934ec07e82f188eac93b9328378
Hari-repo <hariharanuk810@gmail.com>
2025-05-28 22:09:52 +0530

    Git stash tested with this code

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Welcome to the world of java");
+        System.out.println("Welcome to the world of java-stash");
     }
 }
 
