94852b1ae616e9f415f7727c073b91415f0b8f22
Hari-repo <hariharanuk810@gmail.com>
2025-05-30 22:04:28 +0530

    New objs created and tested in anonymous inner class using abstract class

--- src/anonymousInnerClassUsingAbstractClass/ParentChildRelationship.java
+++ src/anonymousInnerClassUsingAbstractClass/ParentChildRelationship.java
@@ -8,7 +8,15 @@ public class ParentChildRelationship {
                 System.out.println("No need to create child class with extented the abstract class only need is inner class");
             }
         };
+
+        Parent obj1 = new Parent() {
+            @Override
+            public void partnerSetting() {
+                System.out.println("New obj overriding");
+            }
+        };
         obj.partnerSetting();
+        obj1.partnerSetting();
     }
 }
 
